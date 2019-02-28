e5ced9d7c9c3ec1b5786717e1540560808f6eba6
Lawrence D'Anna <larry@elder-gods.org>
2019-02-27 23:15:20 -0800

    LED and serial working

---
+++ breadboard.ino
@@ -0,0 +1,50 @@
+#define LED 9
+
+#include <SoftwareSerial.h>
+
+// ATMEL ATMEGA8 & 168 / ARDUINO
+//
+//                  +-\/-+
+//            PC6  1|    |28  PC5 (AI 5)   (arduino pin 19)
+//      (D 0) PD0  2|    |27  PC4 (AI 4)   (arduino pin 18)
+//      (D 1) PD1  3|    |26  PC3 (AI 3)
+//      (D 2) PD2  4|    |25  PC2 (AI 2)
+// PWM+ (D 3) PD3  5|    |24  PC1 (AI 1)
+//      (D 4) PD4  6|    |23  PC0 (AI 0)
+//            VCC  7|    |22  GND
+//            GND  8|    |21  AREF
+//            PB6  9|    |20  AVCC
+//            PB7 10|    |19  PB5 (D 13)
+// PWM+ (D 5) PD5 11|    |18  PB4 (D 12)
+// PWM+ (D 6) PD6 12|    |17  PB3 (D 11) PWM
+//      (D 7) PD7 13|    |16  PB2 (D 10) PWM
+//      (D 8) PB0 14|    |15  PB1 (D 9) PWM
+//                  +----+
+
+
+SoftwareSerial softserial(18, 19); // RX, TX
+
+void setup()
+{
+  pinMode(19, OUTPUT);
+  pinMode(18, INPUT);
+  
+  softserial.begin(9600); 
+  pinMode(LED, OUTPUT);
+  for (int i = 0; i < 5; i++) { 
+    digitalWrite(LED, HIGH);
+    delay(200);
+    digitalWrite(LED, LOW);
+    delay(200); 
+  }
+  delay(2000);
+}
+
+void loop()
+{
+  delay(1000);
+  softserial.write("test, 1 2 3 4\n");
+  digitalWrite(LED, HIGH);
+  delay(100); 
+  digitalWrite(LED, LOW);
+}
